9f2ea59f9a4d022b432896cca6d3d64ba8d443d6
Livni <llmc0383@gmail.com>
2023-07-18 10:32:55 -0600

    se comentarea que realiza cada evento con los diferentes botones

--- script.js
+++ script.js
@@ -2,30 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //Boton de busqueda de personajes 
     document.getElementById("boton-busqueda-personajes").addEventListener("click", function() {
-      var terminoBusquedaPersonajes = document.getElementById("input-busqueda-personajes").value;
-      obtenerPersonajes(terminoBusquedaPersonajes);
+     // Obtener el término de búsqueda de personajes desde el campo de entrada  
+        var terminoBusquedaPersonajes = document.getElementById("input-busqueda-personajes").value;
+      // Llamar a la función obtenerPersonajes pasando el término de búsqueda 
+        obtenerPersonajes(terminoBusquedaPersonajes);
     });
     //Boton de busqueda por episodios
     document.getElementById("boton-busqueda-episodios").addEventListener("click", function() {
-      var terminoBusquedaEpisodios = document.getElementById("input-busqueda-episodios").value;
-      obtenerEpisodios(terminoBusquedaEpisodios);
+     // Obtener el término de búsqueda de episodios desde el campo de entrada  
+        var terminoBusquedaEpisodios = document.getElementById("input-busqueda-episodios").value;
+    // Llamar a la función obtenerEpisodios pasando el término de búsqueda 
+        obtenerEpisodios(terminoBusquedaEpisodios);
     });
     //Boton de busqueda por ubicaciones
     document.getElementById("boton-busqueda-ubicaciones").addEventListener("click", function() {
-      var terminoBusquedaUbicaciones = document.getElementById("input-busqueda-ubicaciones").value;
-      obtenerUbicaciones(terminoBusquedaUbicaciones);
+    // Obtener el término de búsqueda de ubicaciones desde el campo de entrada
+        var terminoBusquedaUbicaciones = document.getElementById("input-busqueda-ubicaciones").value;
+    // Llamar a la función obtenerUbicaciones pasando el término de búsqueda  
+        obtenerUbicaciones(terminoBusquedaUbicaciones);
     });
     //Boton de para limpiar personajes 
     document.getElementById("boton-limpiar-personajes").addEventListener("click", function() {
+    // Llamar a la función limpiarTabla pasando el ID de la tabla de personajes  
       limpiarTabla("tabla-personajes");
     });
     //Boton de para limpiar espisodios
     document.getElementById("boton-limpiar-episodios").addEventListener("click", function() {
-      limpiarTabla("tabla-episodios");
+    // Llamar a la función limpiarTabla pasando el ID de la tabla de episodios 
+        limpiarTabla("tabla-episodios");
     });
     //Boton de para limpiar ubicaciones
     document.getElementById("boton-limpiar-ubicaciones").addEventListener("click", function() {
-      limpiarTabla("tabla-ubicaciones");
+    // Llamar a la función limpiarTabla pasando el ID de la tabla de ubicaciones
+        limpiarTabla("tabla-ubicaciones");
     });
 
 });
